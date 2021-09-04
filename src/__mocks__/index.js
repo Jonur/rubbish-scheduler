f64b81464d5500d3400c643dea2cfd75725eb281
Dimitris Damilos <d.k.damilos@gmail.com>
2021-09-04 13:47:03 +0100

    Add puppeteer response mock

---
+++ src/__mocks__/index.js
@@ -0,0 +1,20 @@
+const puppeteerResponse = [
+  { title: "Food Waste", nextCollectionDate: "9 September 2021" },
+  {
+    title: "Paper & Cardboard",
+    nextCollectionDate: "9 September 2021",
+  },
+  {
+    title: "Non-Recyclable Refuse",
+    nextCollectionDate: "9 September 2021",
+  },
+  { title: "Garden Waste", nextCollectionDate: "13 September 2021" },
+  {
+    title: "Mixed Recycling (Cans, Plastics & Glass)",
+    nextCollectionDate: "16 September 2021",
+  },
+];
+
+module.exports = {
+  puppeteerResponse,
+};
