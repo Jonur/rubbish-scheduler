d8c4e94eefdb299808f1157ed01e40c24908cf57
Dimitrios Konstantinos Damilos <d.k.damilos@gmail.com>
2022-01-03 11:34:54 +0000

    Add colourIds records

--- libs/constants.js
+++ libs/constants.js
@@ -22,6 +22,20 @@ const MONTHS = [
 const USER_PREFERENCES = {
   TIMEZONE: 'Europe/London',
   LOCATION: process.env.USER_LOCATION,
+  /**
+   * Google Calendar event colour IDs
+   * 1 blue
+   * 2 green
+   * 3 purple
+   * 4 red
+   * 5 yellow
+   * 6 orange
+   * 7 turquoise
+   * 8 gray
+   * 9 bold blue
+   * 10 bold green
+   * 11 bold red
+   */
   GOOGLE_CALENDAR_EVENT_COLOUR_ID: '6',
   CALENDAR_ID: process.env.GOOGLE_CALENDAR_ID,
 };
