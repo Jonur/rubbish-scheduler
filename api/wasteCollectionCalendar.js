055f9902d5b600780c0bd6902e976a5580bdac6c
Dimitrios Konstantinos Damilos <d.k.damilos@gmail.com>
2022-01-03 11:24:02 +0000

    Create API call

---
+++ api/wasteCollectionCalendar.js
@@ -0,0 +1,27 @@
+const { google } = require('googleapis');
+
+const {
+  createCalendarEvents,
+  deleteExistingCalendarEvents,
+  getExistingCalendarEvents,
+  getShouldBeUsingMocks,
+  getWasteCollectionsData,
+  insertEventsIntoCalendar,
+  OAuth2Client,
+} = require('../src/utils');
+
+const wasteCollectionCalendar = async () => {
+  const calendar = google.calendar({ version: 'v3' });
+
+  const wasteCollectionsData = getShouldBeUsingMocks() || (await getWasteCollectionsData());
+
+  const calendarEvents = createCalendarEvents(wasteCollectionsData);
+
+  const existingCalendarEvents = await getExistingCalendarEvents(OAuth2Client, calendar, calendarEvents);
+
+  await deleteExistingCalendarEvents(OAuth2Client, calendar, existingCalendarEvents);
+
+  insertEventsIntoCalendar(OAuth2Client, calendar, calendarEvents);
+};
+
+module.exports = wasteCollectionCalendar;
--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-const { google } = require('googleapis');
+const wasteCollectionCalendar = require('./api/wasteCollectionCalendar');
 
-const {
-  createCalendarEvents,
-  deleteExistingCalendarEvents,
-  getExistingCalendarEvents,
-  getShouldBeUsingMocks,
-  getWasteCollectionsData,
-  insertEventsIntoCalendar,
-  OAuth2Client,
-} = require('./src/utils');
-
-(async () => {
-  const calendar = google.calendar({ version: 'v3' });
-
-  const wasteCollectionsData = getShouldBeUsingMocks() || (await getWasteCollectionsData());
-
-  const calendarEvents = createCalendarEvents(wasteCollectionsData);
-
-  const existingCalendarEvents = await getExistingCalendarEvents(OAuth2Client, calendar, calendarEvents);
-
-  await deleteExistingCalendarEvents(OAuth2Client, calendar, existingCalendarEvents);
-
-  insertEventsIntoCalendar(OAuth2Client, calendar, calendarEvents);
-})();
+wasteCollectionCalendar();
