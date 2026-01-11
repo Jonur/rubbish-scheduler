d57f025f8b6e50b40c8a5cf79c59209ca1a233c5
Jonur <d.k.damilos@gmail.com>
2026-01-11 13:40:14 +0000

    Update file folder structure

--- src/api/wasteCollectionCalendar.js
+++ src/api/wasteCollectionCalendar.js
@@ -1,13 +1,13 @@
 const { google } = require('googleapis');
 
-const assertCronAuth = require('../helpers/assertCronAuth');
-const getShouldBeUsingMocks = require('../helpers/getShouldBeUsingMocks');
-const getWasteCollectionsData = require('../helpers/getWasteCollectionsData');
-const createCalendarEvents = require('../helpers/createCalendarEvents');
-const deleteExistingCalendarEvents = require('../helpers/deleteExistingCalendarEvents');
-const OAuth2Client = require('../helpers/getOAuth2Client');
-const getExistingCalendarEvents = require('../helpers/getExistingCalendarEvents');
-const insertEventsIntoCalendar = require('../helpers/insertEventsIntoCalendar');
+const assertCronAuth = require('../lib/assertCronAuth');
+const getShouldBeUsingMocks = require('../lib/getShouldBeUsingMocks');
+const getWasteCollectionsData = require('../lib/getWasteCollectionsData');
+const createCalendarEvents = require('../lib/createCalendarEvents');
+const deleteExistingCalendarEvents = require('../lib/deleteExistingCalendarEvents');
+const OAuth2Client = require('../lib/getOAuth2Client');
+const getExistingCalendarEvents = require('../lib/getExistingCalendarEvents');
+const insertEventsIntoCalendar = require('../lib/insertEventsIntoCalendar');
 
 const wasteCollectionCalendar = async (req, res) => {
   try {
--- src/api/wasteCollectionLocalRun.js
+++ src/api/wasteCollectionLocalRun.js
@@ -1,12 +1,12 @@
 const { google } = require('googleapis');
 
-const getShouldBeUsingMocks = require('../helpers/getShouldBeUsingMocks');
-const getWasteCollectionsData = require('../helpers/getWasteCollectionsData');
-const createCalendarEvents = require('../helpers/createCalendarEvents');
-const deleteExistingCalendarEvents = require('../helpers/deleteExistingCalendarEvents');
-const OAuth2Client = require('../helpers/getOAuth2Client');
-const getExistingCalendarEvents = require('../helpers/getExistingCalendarEvents');
-const insertEventsIntoCalendar = require('../helpers/insertEventsIntoCalendar');
+const getShouldBeUsingMocks = require('../lib/getShouldBeUsingMocks');
+const getWasteCollectionsData = require('../lib/getWasteCollectionsData');
+const createCalendarEvents = require('../lib/createCalendarEvents');
+const deleteExistingCalendarEvents = require('../lib/deleteExistingCalendarEvents');
+const OAuth2Client = require('../lib/getOAuth2Client');
+const getExistingCalendarEvents = require('../lib/getExistingCalendarEvents');
+const insertEventsIntoCalendar = require('../lib/insertEventsIntoCalendar');
 
 (async () => {
   const calendar = google.calendar({ version: 'v3', auth: OAuth2Client });
